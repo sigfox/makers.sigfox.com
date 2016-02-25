98f099aab566a4ef8ecfc5c58b791a74cd1f5670
nicolsc <nicolas.lesconnec@gmail.com>
2016-02-25 12:25:27 +0100

    Fix newsletter subscription

--- js/sigfox.js
+++ js/sigfox.js
@@ -1,40 +1,28 @@
 (function(){
   $(document).ready(function() {
-   // initNav();
+   checkNewsletterStatus();
   });
-  
-  function initNav(){
-    var nav = '#main-nav';
-    initScrollSpy(nav);
-    initNavSpy(nav);
+  function checkNewsletterStatus(){
+    var qs = getQueryString();
+    console.log(qs);
+    if (qs && qs.nconf==="1"){
+      displayNewsletterConfirmationMsg();
+    }
   }
-  function initScrollSpy(selector){
-    $('body').scrollspy({ target: selector });
-    
-    
-    $(selector).on('activate.bs.scrollspy', function () {
-      var hash = $(this).find("li.active:last a").attr("href");//.replace(/^#/, '');
-      pushState(hash);
-    });
-  }
-  function initNavSpy(selector){
-    $(selector+' a').bind('click', function(evt) {
-      var hash = /#(.+)$/.exec(evt.currentTarget.href);
-      if (!hash){
-        return true;
+  function getQueryString(){
+    var querystring = {};
+    window.location.search.substr(1).split('&').forEach(function(qs_entry){
+      var parts = qs_entry.split("=");
+      if (parts.length !== 2){
+        return null;
       }
-      hash = hash.shift(); 
-      pushState(hash);
-      $('html,body').stop().animate({
-          scrollTop: $(hash).offset().top - 50
-      }, 1000);
-      
-      evt.preventDefault();
-      return false;
+      querystring[parts[0]] = parts[1];
     });
+    
+    return querystring;
   }
-  function pushState(hash){
-    window.history.pushState(hash, hash, hash);
+  function displayNewsletterConfirmationMsg(){
+    $('#newsletter-form form').hide();
+    $('#newsletter-form .message').show();
   }
-  
 })();
